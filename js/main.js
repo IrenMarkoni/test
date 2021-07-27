8c0a9014ae951634fbf01803488dfbec1b2e3990
IrenMarkoni <77893647+IrenMarkoni@users.noreply.github.com>
2021-07-27 10:13:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@ let openModal = document.querySelector('.open_modal');
 let modalWindow = document.querySelector('.modal_window');
 let closeModal = document.getElementById('modal_box_close');
 
-[].forEach.call( openModal, function(el) {
+/*[].forEach.call( openModal, function(el) {
 
     el.onclick = function(e) {
      modalWindow.style.display = 'block'; 
     }
-});
+});*/
 
-
+openModal.onclick = function(){
+   modalWindow.style.display = 'block';
+}
 closeModal.onclick = function() {
    modalWindow.style.display = 'none'; 
 }
